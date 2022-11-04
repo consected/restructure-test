1e6b75ebe088cf78ade694673586355338671204
Phil <phil.ayres@consected.com>
2022-11-04 12:16:42 +0000

    Updated sample build-vars to use latest geckodriver

--- shared/build-vars-sample.sh
+++ shared/build-vars-sample.sh
@@ -7,8 +7,21 @@ DOCS_REPO_URL="https://github.com/somerep_for_docs"
 DB_USER=$(whoami)
 DB_PASSWORD=root
 
+# The geckodriver is required for Selenium to run against Firefox.
 # See geckodriver releases at: https://github.com/mozilla/geckodriver/releases
-GECKODRIVER=https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
+# It is possible that a different version will be required based on the architecture of the machine. Others available are
+# geckodriver-v0.32.0-linux-aarch64.tar.gz
+# geckodriver-v0.32.0-linux-aarch64.tar.gz.asc
+# geckodriver-v0.32.0-linux32.tar.gz
+# geckodriver-v0.32.0-linux32.tar.gz.asc
+# geckodriver-v0.32.0-linux64.tar.gz
+# geckodriver-v0.32.0-linux64.tar.gz.asc
+# geckodriver-v0.32.0-macos-aarch64.tar.gz
+# geckodriver-v0.32.0-macos.tar.gz
+# geckodriver-v0.32.0-win-aarch64.zip
+# geckodriver-v0.32.0-win32.zip
+
+GECKODRIVER=https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz
 
 # Uncomment if you want to drop and recreate the test database on startup
 # DROP_DATABASE=true
